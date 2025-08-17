20877650c4dbe55b4f4eae0ff2d23813328579e5
realslimtaek <xorgud777@gmail.com>
2025-08-17 14:12:27 +0900

    init

---
+++ src/main/kotlin/com/assignment/greencatsoft/GreencatsoftApplication.kt
@@ -0,0 +1,11 @@
+package com.assignment.greencatsoft
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GreencatsoftApplication
+
+fun main(args: Array<String>) {
+    runApplication<GreencatsoftApplication>(*args)
+}
---
+++ src/test/kotlin/com/assignment/greencatsoft/GreencatsoftApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.assignment.greencatsoft
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GreencatsoftApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
