0f7d150d7cafeaba7d93d36875b94a1ef8d9d431
realslimtaek <xorgud777@gmail.com>
2025-08-17 23:08:12 +0900

    feat: 도메인 추가에 의한 Mapper Logic 추가

--- src/main/kotlin/com/assignment/greencatsoft/adaptor/out/user/Mapper.kt
+++ src/main/kotlin/com/assignment/greencatsoft/adaptor/out/user/Mapper.kt
@@ -1,6 +1,26 @@
 package com.assignment.greencatsoft.adaptor.out.user
 
+import com.assignment.greencatsoft.domain.user.User
+import com.assignment.greencatsoft.domain.user.UserImpl
 import org.springframework.stereotype.Component
 
 @Component
-object UsersMapper
+object UsersMapper {
+
+    fun toDomain(entity: UserEntity): User = UserImpl(
+        id = entity.id,
+        email = entity.email,
+        password = entity.password,
+        name = entity.name,
+        status = entity.status,
+    )
+
+    fun toEntity(domain: User) = UserEntity(
+        email = domain.email,
+        password = domain.password,
+        name = domain.name,
+        status = domain.status,
+    ).apply {
+        this.id = domain.id
+    }
+}
